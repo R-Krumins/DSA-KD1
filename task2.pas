460b31f18b88a1e69e7e6a301ad6ce0cd360b962
Rainers Krumins <rainerkrum@gmail.com>
2024-10-24 04:40:32 +0300

    task2

--- task2.pas
+++ task2.pas
@@ -41,6 +41,34 @@ begin
       end;
 end;
 begin
+   songs[1].author := 'Artist A';
+   songs[1].name := 'Rock Balad';
+   songs[1].genre := 'rock';
+   songs[1].year := 2020;
+   songs[1].month := 6;
+   songs[1].length := 210;
+
+   songs[2].author := 'Artist B';
+   songs[2].name := 'Jazz Nights';
+   songs[2].genre := 'jazz';
+   songs[2].year := 2019;
+   songs[2].month := 5;
+   songs[2].length := 300;
+
+   songs[3].author := 'Artist C';
+   songs[3].name := 'Back in Black';
+   songs[3].genre := 'rock';
+   songs[3].year := 2021;
+   songs[3].month := 8;
+   songs[3].length := 240;
+
+   songs[4].author := 'Artist D';
+   songs[4].name := 'Disco Fever';
+   songs[4].genre := 'disco';
+   songs[4].year := 1979;
+   songs[4].month := 11;
+   songs[4].length := 180;
+
    A();
    B();
 end.
